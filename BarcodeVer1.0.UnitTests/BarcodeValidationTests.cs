507b55a39172bbbde4fe543b0d85d960fef28953
vothikimchi <vtkchi1297@gmail.com>
2018-05-28 23:25:49 +0700

    add project unit test

---
+++ BarcodeVer1.0.UnitTests/BarcodeValidationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BarcodeVer1._0.UnitTests
+{
+    [TestClass]
+    public class BarcodeValidationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
