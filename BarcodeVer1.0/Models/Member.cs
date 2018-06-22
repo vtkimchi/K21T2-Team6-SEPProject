908e57d658706ff3719a09c37d1fb2f3cc97fd8f
PhamCongDuc2905 <zea_0107@yahoo.com.vn>
2018-06-22 22:58:13 +0700

    update view student profile

--- BarcodeVer1.0/Models/Member.cs
+++ BarcodeVer1.0/Models/Member.cs
@@ -25,6 +25,7 @@ namespace BarcodeVer1._0.Models
         public string MaKH { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public Nullable<System.DateTime> Birthday { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Attendance> Attendances { get; set; }
---
+++ BarcodeVer1.0/Models/ResponseData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BarcodeVer1._0.Models
+{
+    public class ResponseData
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+}
